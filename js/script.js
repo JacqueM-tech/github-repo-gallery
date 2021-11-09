dd48e5a5839e781cca3789a7aa72a0db91e8b906
JacqueM <ljacque16@gmail.com>
2021-11-09 15:02:59 -0600

    updated JS file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // This div is where your profile information will appear
 const overview = document.querySelector(".overview");
 const username = "JacqueM-tech";
+const repoList = document.querySelector("repo-list");
 
 // async function to fetch information from my GitHub profile using the GitHub API address: https://api.github.com. Target the “users” endpoint and use a template literal to add the global username variable to the endpoint: users/${username}
 const getProfileInfo = async function () {
@@ -9,7 +10,7 @@ const getProfileInfo = async function () {
     const data = await profileInfo.json();
     displayProfileInfo(data);
 };
-// console.log(getProfileInfo());
+getProfileInfo();
 
 // Below the async function to fetch your GitHub user data, create and name a new function to display the fetched user information on the page. This function should accept the JSON data as a parameter.Inside the function, create a new div and give it a class of “user-info”. Using innerHTML, populate the div, with the elements for figure, image, and paragraphs: 
 
@@ -30,3 +31,14 @@ const displayProfileInfo = function (data) {
 
     overview.append(div);
 };
+
+// At the bottom of your code, create and name a new async function to fetch your repos. Use the List Repositories of User(opens in a new tab) section of the documentation to find the endpoints for your API URL to fetch the list of repos.
+const getReposList = async function () {
+    const reposList = await fetch (`https://api.github.com/users/${username}/repos?sort=update&per_page=100`); 
+    const repoData = await reposList.json();
+    // displayReposList(repoData);
+};
+// getReposList();
+
+const repoInfo = function (repos) {};
+
